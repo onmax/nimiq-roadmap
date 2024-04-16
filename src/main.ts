c768a2e894bc1faf7bccdad665fa69918e431bf9
Maxi <maximogarciamtnez@gmail.com>
2024-04-16 19:05:45 +0200

    fixed buikd

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,9 @@ import { createPinia } from 'pinia'
 import 'virtual:uno.css'
 
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
 
 app.mount('#app')
