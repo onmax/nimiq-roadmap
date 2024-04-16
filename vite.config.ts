1c2864adf7d05016265f019e3a622e9115c2b340
Maxi <maximogarciamtnez@gmail.com>
2024-04-16 18:59:25 +0200

    added deployment

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'nimiq-roadmap',
   plugins: [
     vue(),
     VueDevTools(),
